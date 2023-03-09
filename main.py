e46350ebcb4795df355b9fc70082be42c870caf2
Akiva Schiff <akiva@riseup.co.cil>
2023-03-09 13:39:31 +0200

    Focus to infinity

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from picamera2 import Picamera2, Preview
+from libcamera import controls
 import time
 
 picam2 = Picamera2()
 
 picam2.configure(picam2.still_configuration)
 picam2.start_preview(Preview.NULL)
-time.sleep(2)
+
+# set focus manually to infinity
+# for some reason, autofocus doesn't work for me with no preview and stills capture
+picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0})
+
+# wait one second
+time.sleep(1)
 picam2.start()
 
 # Loop forever
@@ -16,8 +23,8 @@ while True:
     # Take a picture and save it with the timestamp in the filename
     picam2.capture_file(f"images/image_{current_time}.jpg")
 
-    # Wait for 2 minutes
-    time.sleep(120)
+    # Wait for 30 seconds
+    time.sleep(30)
 
 
 picam2.stop_preview()
