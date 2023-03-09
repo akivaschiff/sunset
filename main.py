c91e2055c5d2fe4890b32bb0ace3ea6dd06a41f4
Akiva Schiff <akiva@riseup.co.cil>
2023-03-09 11:37:00 +0200

    Look for picture every 30 seconds

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from picamera2 import Picamera2, Preview
 import time
-print('hello')
+
 picam2 = Picamera2()
 
 picam2.configure(picam2.still_configuration)
 picam2.start_preview(Preview.NULL)
 time.sleep(2)
 picam2.start()
-picam2.capture_file("capture.jpg")
+
+# Loop forever
+while True:
+    # Get the current time
+    current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Take a picture and save it with the timestamp in the filename
+    picam2.capture_file(f"image_{current_time}.jpg")
+
+    # Wait for 2 minutes
+    time.sleep(30)
+
 
 picam2.stop_preview()
 picam2.stop()
