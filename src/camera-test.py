0cd97fe4cbf292fb00729b2d3b8186144725869c
Akiva Schiff <akiva@riseup.co.cil>
2023-03-15 09:02:34 +0200

    Add camera and test-camera modules

---
+++ src/camera-test.py
@@ -0,0 +1,9 @@
+
+def init():
+	print('starting test camera...')
+
+def capture(filename):
+    print(f'capturing image to {filename}')
+
+def destroy():
+	print('stopping test camera...')
---
+++ src/camera.py
@@ -0,0 +1,24 @@
+from picamera2 import Picamera2, Preview
+from libcamera import controls
+
+def init():
+	picam2 = Picamera2()
+
+	# picam2.configure(picam2.still_configuration) - swapped this for the following line where I can control the size
+	picam2.configure(picam2.create_still_configuration(main={"size": (1920, 1080)}))
+	picam2.start_preview(Preview.NULL)
+
+	# set focus manually to infinity
+	# for some reason, autofocus doesn't work for me with no preview and stills capture
+	picam2.set_controls({ "AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0 })
+
+	# wait one second
+	time.sleep(1)
+	picam2.start()
+
+def capture(filename):
+    picam2.capture_file(filename)
+
+def destroy():
+	picam2.stop_preview()
+	picam2.stop()
