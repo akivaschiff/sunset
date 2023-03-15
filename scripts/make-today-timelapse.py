2637fe0a970f11cd034caf28b28ef52ab35d3084
Akiva Schiff <akiva@riseup.co.cil>
2023-03-15 23:01:26 +0200

    Save timelapse in correct folder

--- scripts/make-today-timelapse.py
+++ scripts/make-today-timelapse.py
@@ -1,17 +1,34 @@
 import cv2
 import os
+import sys
+from datetime import datetime
+
+date_prefix = datetime.now().strftime("%Y-%m-%d") if len(sys.argv) == 1 else sys.argv[1]
+
+# create timelapse directory
+directory = './timelapses'
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 # specify the path to the folder containing the images
-image_folder = '/Users/akivaschiff/myprojects/sunset/images/results-storm'
+image_folder = './images'
 
 # specify the desired frame rate of the output video
 fps = 30.0
 
 # get a list of all the image filenames in the folder
-image_filenames = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith('.jpg') and f[:len('image_2023-03-14_16-16-54')] >= 'image_2023-03-14_16-16-54']
+image_filenames = [os.path.join(image_folder, f) for f in os.listdir(image_folder) if f.endswith('.jpg') and f.startswith(f'image_{date_prefix}')]
 
 # sort the image filenames in alphabetical order
 image_filenames.sort()
+
+if len(image_filenames) == 0:
+	print('Could not find any images')
+	sys.exit(0)
+
+video_file_name = os.path.join(directory, f'{date_prefix}.mp4')
+print(f'creating video from {len(image_filenames)} images at {video_file_name}')
 
 # open the first image to get the image size
 img = cv2.imread(image_filenames[0])
@@ -19,7 +36,7 @@ height, width, channels = img.shape
 
 # create a video writer object with the specified frame rate and size
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') # specify the codec for the output video
-out = cv2.VideoWriter('/Users/akivaschiff/myprojects/sunset/output-storm-small.mp4', fourcc, fps, (width, height))
+out = cv2.VideoWriter(video_file_name, fourcc, fps, (width, height))
 
 # loop through each image in the folder and write it to the video
 for image_filename in image_filenames:
